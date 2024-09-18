72f55425ec631e1365734cbe2d212518274177de
akmal0274 <hafizhakmal20@gmail.com>
2024-09-18 21:34:59 +0700

    add supabase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:fnb_billiard/views/homepage.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+
+
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(const MyApp());
 }
 
@@ -17,7 +28,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Homepage()
+      home: Homepage()
     );
   }
 }
